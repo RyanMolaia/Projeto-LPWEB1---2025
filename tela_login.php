8fcbbebacf8262dd20c9db243bfbfd579a5b2720
Lucas Munhoz <149180699+Elmunhas2004@users.noreply.github.com>
2025-05-06 18:35:18 -0300

    Create tela_login.php

---
+++ tela_login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN</title>
+    <link rel="stylesheet" href="logon.css">
+</head>
+<body>
+    <div class="login-container">
+        <h2>Login<h2>
+            <form action="#" method="post">
+                <div class="input-group">
+                    <label for="usuario">User:</label>
+                    <input type="text" id="usuario" name="usuario" required>
+                </div>
+                
+                <div class="input-group">
+                    <label for="senha">Password:</label>
+                    <input type="text" id="senha" name="senha" required>
+                </div>            
+                
+                <div class="buttom-container">
+                    <button type="submit">Entrar</button>
+                </div>            
+            </div>
+    </div>
+</body>
+</html>
