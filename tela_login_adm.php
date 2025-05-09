3279a1375681c27070d256a52499550ff5da203c
Lucas Munhoz <149180699+Elmunhas2004@users.noreply.github.com>
2025-05-09 18:37:53 -0300

    Create tela_login_adm.php

---
+++ tela_login_adm.php
@@ -0,0 +1,36 @@
+<?php include("banco.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="logon.css">
+</head>
+<body>
+    <div class="login-container">
+        <div class="title">
+            <h2>Login- ADM</h2>
+        </div>
+        <div class="mensage-attention">
+            <div>
+                <h4>Atenção!</h4>
+                <p>Caso deseje criar ou acessar usuário de Administrador</p>
+                <p>por favor consutar suporte ou equipe de TI</p>
+            </div>
+        </div>
+
+            <form action="#" method="post">
+                <div class="input-group">
+                    <input type="text" id="usuario" name="usuario" placeholder="👤 Usuário" required>
+                    <input type="text" id="senha" name="senha" placeholder="🔑 Senha" required>
+                </div>   
+                <div class="button-container">
+
+                    <button type="submit">Entrar</button>
+
+                </div>
+            </form>
+    </div>
+</body>
+</html>
