2f97664104654787e9fafa77513e729c8bd30c13
RyanMolaia <ryan_molaia@hotmail.com>
2025-05-08 21:43:01 -0300

    Add files via upload

---
+++ storecomp.sql
@@ -0,0 +1,184 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 09/05/2025 às 02:34
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `storecomp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nome`) VALUES
+(1, 'Desktop'),
+(2, 'Monitor'),
+(3, 'Notebook'),
+(4, 'Hardware');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `preco` decimal(10,2) NOT NULL,
+  `categorias_id` int(11) DEFAULT NULL,
+  `imagem` blob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id`, `nome`, `preco`, `categorias_id`, `imagem`) VALUES
+(1, 'Notebook-Gamer-Acer-Nitro-V15-Intel-Core-i5-13-Gera-o-8GB-RTX-3050-SSD-512GB-Tela-15-6-Full-HD-Linux', 4599.00, 3, 0x696d672f6e6f7465626f6f6b2e77656270),
+(2, 'Monitor-gamer-lg-ultragear-24-full-hd-ips-180hz-1ms-displayport-e-hdmi-nvidia-g-sync-amd-freesync-hdr10-srgb-99-24gs60f', 899.99, 2, 0x696d672f6d6f6e69746f722e77656270),
+(3, 'PC-Gamer-BluePC-Legacy-AMD-Ryzen-5-5600GT-16GB-RAM-Radeon-VEGA-7-SSD-480GB-Fonte-500W-PGBP-1001LEG_1744900832', 2399.00, 2, 0x696d672f6465736b746f702e77656270),
+(5, 'Placa-de-Video-rx-7800xt-gaming-16g-xfx-speedster-qick319-radeon-16gb-ddr6-hdmi-3xdp-3-fan-rx-78tqickf9_1719595732_m', 3899.99, 4, 0x696d672f506c6163612d64652d566964656f2d52782d47616d696e672e77656270),
+(6, 'Water-Cooler-Husky-Freezy-argb-240mm-amd-e-intel-preto-hwt200pt_1732644962_m', 207.90, 4, 0x696d672f57617465722d436f6f6c65722d4875736b792e77656270);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `usuario` varchar(100) NOT NULL,
+  `senha` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `vendas`
+--
+
+CREATE TABLE `vendas` (
+  `id` int(11) NOT NULL,
+  `data_venda` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `vendas_itens`
+--
+
+CREATE TABLE `vendas_itens` (
+  `id` int(11) NOT NULL,
+  `vendas_id` int(11) DEFAULT NULL,
+  `produtos_id` int(11) DEFAULT NULL,
+  `quantidade` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categorias_id` (`categorias_id`);
+
+--
+-- Índices de tabela `vendas`
+--
+ALTER TABLE `vendas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `vendas_itens`
+--
+ALTER TABLE `vendas_itens`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `vendas_id` (`vendas_id`),
+  ADD KEY `produtos_id` (`produtos_id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de tabela `vendas`
+--
+ALTER TABLE `vendas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `vendas_itens`
+--
+ALTER TABLE `vendas_itens`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `produtos`
+--
+ALTER TABLE `produtos`
+  ADD CONSTRAINT `produtos_ibfk_1` FOREIGN KEY (`categorias_id`) REFERENCES `categorias` (`id`);
+
+--
+-- Restrições para tabelas `vendas_itens`
+--
+ALTER TABLE `vendas_itens`
+  ADD CONSTRAINT `vendas_itens_ibfk_1` FOREIGN KEY (`vendas_id`) REFERENCES `vendas` (`id`),
+  ADD CONSTRAINT `vendas_itens_ibfk_2` FOREIGN KEY (`produtos_id`) REFERENCES `produtos` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
