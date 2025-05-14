6ed83a9a39594e13cc063732d00c81ceba7de935
RyanMolaia <ryan_molaia@hotmail.com>
2025-05-14 19:49:24 -0300

    Add files via upload

--- storecomp.sql
+++ storecomp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 09/05/2025 às 02:34
+-- Tempo de geração: 15/05/2025 às 00:48
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -53,19 +53,20 @@ CREATE TABLE `produtos` (
   `nome` varchar(255) NOT NULL,
   `preco` decimal(10,2) NOT NULL,
   `categorias_id` int(11) DEFAULT NULL,
-  `imagem` blob DEFAULT NULL
+  `imagem` blob DEFAULT NULL,
+  `qtd_estoque` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `produtos`
 --
 
-INSERT INTO `produtos` (`id`, `nome`, `preco`, `categorias_id`, `imagem`) VALUES
-(1, 'Notebook-Gamer-Acer-Nitro-V15-Intel-Core-i5-13-Gera-o-8GB-RTX-3050-SSD-512GB-Tela-15-6-Full-HD-Linux', 4599.00, 3, 0x696d672f6e6f7465626f6f6b2e77656270),
-(2, 'Monitor-gamer-lg-ultragear-24-full-hd-ips-180hz-1ms-displayport-e-hdmi-nvidia-g-sync-amd-freesync-hdr10-srgb-99-24gs60f', 899.99, 2, 0x696d672f6d6f6e69746f722e77656270),
-(3, 'PC-Gamer-BluePC-Legacy-AMD-Ryzen-5-5600GT-16GB-RAM-Radeon-VEGA-7-SSD-480GB-Fonte-500W-PGBP-1001LEG_1744900832', 2399.00, 2, 0x696d672f6465736b746f702e77656270),
-(5, 'Placa-de-Video-rx-7800xt-gaming-16g-xfx-speedster-qick319-radeon-16gb-ddr6-hdmi-3xdp-3-fan-rx-78tqickf9_1719595732_m', 3899.99, 4, 0x696d672f506c6163612d64652d566964656f2d52782d47616d696e672e77656270),
-(6, 'Water-Cooler-Husky-Freezy-argb-240mm-amd-e-intel-preto-hwt200pt_1732644962_m', 207.90, 4, 0x696d672f57617465722d436f6f6c65722d4875736b792e77656270);
+INSERT INTO `produtos` (`id`, `nome`, `preco`, `categorias_id`, `imagem`, `qtd_estoque`) VALUES
+(1, 'Notebook-Gamer-Acer-Nitro-V15-Intel-Core-i5-13-Gera-o-8GB-RTX-3050-SSD-512GB-Tela-15-6-Full-HD-Linux', 4599.00, 3, 0x696d672f6e6f7465626f6f6b2e77656270, 5),
+(2, 'Monitor-gamer-lg-ultragear-24-full-hd-ips-180hz-1ms-displayport-e-hdmi-nvidia-g-sync-amd-freesync-hdr10-srgb-99-24gs60f', 899.99, 2, 0x696d672f6d6f6e69746f722e77656270, 3),
+(3, 'PC-Gamer-BluePC-Legacy-AMD-Ryzen-5-5600GT-16GB-RAM-Radeon-VEGA-7-SSD-480GB-Fonte-500W-PGBP-1001LEG_1744900832', 2399.00, 1, 0x696d672f6465736b746f702e77656270, 10),
+(5, 'Placa-de-Video-rx-7800xt-gaming-16g-xfx-speedster-qick319-radeon-16gb-ddr6-hdmi-3xdp-3-fan-rx-78tqickf9_1719595732_m', 3899.99, 4, 0x696d672f506c6163612d64652d566964656f2d52782d47616d696e672e77656270, 50),
+(6, 'Water-Cooler-Husky-Freezy-argb-240mm-amd-e-intel-preto-hwt200pt_1732644962_m', 207.90, 4, 0x696d672f57617465722d436f6f6c65722d4875736b792e77656270, 0);
 
 -- --------------------------------------------------------
 
@@ -141,7 +142,7 @@ ALTER TABLE `vendas_itens`
 -- AUTO_INCREMENT de tabela `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `produtos`
