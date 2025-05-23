d80d284b578caa4c35ef5129813a22e5e37ea2a1
Sistemas de Informação <si@LAB02-08.laboratorio.dom>
2025-05-22 21:07:39 -0300

    Começo tela menu_cliente

--- menu_cliente.php
+++ menu_cliente.php
@@ -1,5 +1,37 @@
-<?php
-echo "Esta dentro";
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conta Adminitrativa</title>
+    <link rel="stylesheet" href="suporte.css">
+</head>
+<body>
+    <header>
+        <div class="topo">
+            <div class="esquerda">
+                <a href="#sidebar" id="btn-departamento">☰</a>
+            </div>
+            <div>
+                <a href="loja.php"><img class="logo" src="img/logo.png" alt=""></a>
+            </div>
+            <div class="meio">
+                <h3>Menu</h3>
+            </div>
+            <div class="direita">
+                <a href="carrinho.php">🛒</a>
+            </div>
+    </header>
+            <aside id="sidebar">
+                <a href="#" id="fechar-sidebar">🡸</a>
+                    <ul>
+                        <li><a href="#">Carrinho</a></li>
+                        <li><a href="#">Informações pessoais</a></li>
+                        <li><a href="#">Historico de compras</a></li>
+                    </ul>
+            <div class="suporte">
+                <a href="suporte.php">Suporte</a>
+            </div>
+        </aside>
+</body>
+</html>
