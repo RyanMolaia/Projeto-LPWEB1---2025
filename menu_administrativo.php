6dce7587237043d2fcfa60a9f4bdbeb572149587
Sistemas de Informação <si@LAB02-08.laboratorio.dom>
2025-05-28 19:56:35 -0300

    finalizado 50000

--- menu_administrativo.php
+++ menu_administrativo.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Conta Adminitrativa</title>
-    <link rel="stylesheet" href="suporte.css">
+    <link rel="stylesheet" href="logon.css">
 </head>
 <body>
     <header>
